d20d46fc88922e2f06e2a50447452ca6aac80eeb
tiagoalmeida <tiagoalmeida@bemol.com.br>
2024-07-14 23:05:44 -0300

    Adjust the register comuncation

--- Authentication.Infra.Service/Clients/Keycloak/KeycloakConfig.cs
+++ Authentication.Infra.Service/Clients/Keycloak/KeycloakConfig.cs
@@ -2,12 +2,13 @@ namespace Authentication.Infra.Service.Clients.Keycloak;
 
 public class KeycloakConfig
 {
-    public string ClientId { get; set; } = "admin-cli";
-    public string BaseUrl { get; set; } = "http://localhost:8080/admin/realms/ArtesianWell/";
-    public string BaseUrlAuthorization { get; set; } = "http://localhost:8080/realms/master/";
-    public string ClientSecret { get; set; } = "sZOjz2meNh1CTq4eTFQksZG77GdqFLUe";
+    public string ClientId { get; set; }
+    public string AdminId { get; set; }
+    public string BaseUrl { get; set; }
+    public string BaseUrlAuthorization { get; set; }
+    public string ClientSecret { get; set; }
     public int Timeout { get; set; } = 15;
-    public string GrantType { get; set; } = "password";
-    public string Username { get; set; } = "admin";
-    public string Password { get; set; } = "admin";
+    public string GrantType { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
 }
